af33d12ff4ed9bcdda3d21a8259cdf043b0db83b
Aditya Tyagi <tyagiaditya62@gmail.com>
2018-08-08 19:35:45 -0400

    Commit through IntelliJ

---
+++ src/ProjectJava.java
@@ -0,0 +1,7 @@
+public class ProjectJava {
+    public static void main(String [] args){
+        System.out.println("Hello");
+        for(int i=0; i<=10; i++)
+            System.out.println(i);
+    }
+}
